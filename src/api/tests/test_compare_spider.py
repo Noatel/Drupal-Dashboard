f53f94afae10f907d7b578a51f5534b49586f927
Noah <noah.telussa@notive.nl>
2023-01-19 12:18:47 +0100

    Added test for the deleted state

--- src/api/tests/test_compare_spider.py
+++ src/api/tests/test_compare_spider.py
@@ -47,7 +47,7 @@ class CompareSpiderTest(django.test.TestCase):
         self.spider_content = ContentSpider(urls=pages)
         self.spider_compare = CompareSpider(urls=pages)
 
-    def test_content_crawler(self):
+    def test_content_spider(self):
         """
         Test scenario where getting the content is being compared
         This test is for the UNCHANGED data with the database and live database
@@ -84,3 +84,53 @@ class CompareSpiderTest(django.test.TestCase):
         # Checking if the value in the database is the same as the "live" data
         for result in results:
             self.assertEqual(str(Result.STATUS.UNCHANGED), result.status)
+
+    def test_content_spider_changed_data(self):
+        """
+        Test scenario where getting the content is being compared
+        This test is for the UNCHANGED data with the database and live database
+        """
+
+        blocks = Block.objects.all()
+        content = Content.objects.all()
+
+        self.assertEqual(0, blocks.__len__())
+        self.assertEqual(0, content.__len__())
+
+        # Mock the response
+        response = fake_response(file_name='html/typify.html', url='https://www.typify.com')
+
+        # Activate the spider
+        item = self.spider_content.parse(response, testing=True)
+
+        blocks = Block.objects.all()
+        content = Content.objects.all()
+
+        self.assertEqual(9, blocks.__len__())
+        self.assertEqual(9, content.__len__())
+
+        # Check for 0 test results
+        result = Result.objects.all()
+        self.assertEqual(0, result.__len__())
+
+        # Now we going to change the html file to the other mock file
+        response = fake_response(file_name='html/typify_changed.html', url='https://www.typify.com')
+        item = self.spider_compare.parse(response, testing=True)
+
+        # Check for test results
+        results = Result.objects.all()
+        self.assertEqual(9, results.__len__())
+
+        # Checking if the value in the database is the same as the "live" data
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[0].status)
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[1].status)
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[2].status)
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[3].status)
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[4].status)
+
+        self.assertEqual(str(Result.STATUS.EDITED), results[5].status)
+
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[6].status)
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[7].status)
+        self.assertEqual(str(Result.STATUS.UNCHANGED), results[8].status)
+
